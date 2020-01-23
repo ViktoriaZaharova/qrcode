f63839c7b59cadb5d4716fa5e0095bb9f6ae7b7e
viktoriazaharova <zaharova181114@gmail.com>
2020-01-23 15:35:08 +0200

    add edit

--- app/js/common.js
+++ app/js/common.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
-    $('input').focus(function(){
+    $('.password-wrapper > .password-control').click(function(){
+        if ($('[name="password"]').attr('type') === 'password'){
+            $(this).addClass('view');
+            $('[name="password"]').attr('type', 'text');
+        } else {
+            $(this).removeClass('view');
+            $('[name="password"]').attr('type', 'password');
+        }
+        return false;
+    });
+
+    $('[name="password"]').focus(function(){
         $(this).data('placeholder', $(this).attr('placeholder'));
         $(this).attr('placeholder','');
         $('[name="password"]').attr('type', 'password');
     });
-    $('input').blur(function(){
+    $('[name="password"]').blur(function(){
         $(this).attr('placeholder',$(this).data('placeholder'));
-        $('[name="password"]').attr('type', 'text');
+        // $('[name="password"]').attr('type', 'text');
     });
 
     $('.btn-search').click(function () {
@@ -22,22 +33,12 @@ $(document).ready(function() {
        $('body').addClass('open');
     });
 
-    // $('.open').click(function () {
-    //     $('.mobile-menu').css('left', '-100%');
-    //     $('body').removeClass('open');
-    // });
+
 });
 
-$('body').on('click', '.password-control', function(){
-    if ($('[name="password"]').attr('type') === 'password'){
-        $(this).addClass('view');
-        $('[name="password"]').attr('type', 'text');
-    } else {
-        $(this).removeClass('view');
-        $('[name="password"]').attr('type', 'password');
-    }
-    return false;
-});
+
+
+
 
 $(document).mouseup(function (e){ // событие клика по веб-документу
     var div = $(".mobile-menu"); // тут указываем ID элемента
